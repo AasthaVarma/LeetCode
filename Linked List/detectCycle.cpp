19a133176a5010f5e59f6c57aca8598c2630329d
AasthaVarma <34837646+AasthaVarma@users.noreply.github.com>
2020-10-25 19:18:31 +0530

    Create detectCycle.cpp

---
+++ Linked List/detectCycle.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        if(!head){
+            return NULL;
+        }
+        ListNode *slow=head, *fast=head;
+        while(fast && fast->next){
+            slow = slow->next;
+            fast = fast->next->next;
+            if(slow==fast){
+                ListNode *ptr = head;
+                while(true){
+                    if(ptr == slow)
+                        return ptr;
+                    ptr = ptr->next;
+                    slow = slow->next;
+                }
+            }
+        }
+        return NULL;
+    }
+};
